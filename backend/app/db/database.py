4f3b89a3cf026ee2057510f85f4fe7a00f915567
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-02-05 23:15:52 -0300

    fix(db): update connection string attribute name

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,17 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from app.core.config import settings
-from app.models.user_model import User
-
+from app.models.user_model import User 
 
 async def init_db():
-
-    client = AsyncIOMotorClient(str(settings.mongo_uri))
+    client = AsyncIOMotorClient(str(settings.mongo_uri_computed))
     
+    # Seleciona o banco de dados
+    db = client[settings.MONGO_DB]
     
-    await init_beanie(
-        database=client[settings.MONGO_DB], #type: ignore
-        document_models=[
-            User,
-        ]
-    )
+    # Inicializa o Beanie com os modelos
+    await init_beanie(database=db, document_models=[User])
--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://user-authentication-roan.vercel.app/",
 ]
 
 app.add_middleware(
