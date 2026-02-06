cc50eec38d76c460ea5f30ffca0f3da53dbe6ed1
pablomigueldias <pablo.miguel.dias@gmail.com>
2026-02-05 23:04:50 -0300

    chore(deploy): configure mongodb atlas connection and cors for production

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,5 +1,7 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import MongoDsn, computed_field
+from typing import Optional
+
 
 
 class Settings(BaseSettings):
@@ -14,17 +16,22 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    MONGO_HOST: str = 'localhost'
-    MONGO_PORT: int = 27017
-    MONGO_USER: str = 'admin'
-    MONGO_PASS: str = 'password'
-    MONGO_DB: str = 'auth_db'
+    MONGO_URI: Optional[str] = None
 
-    API_V1_STR: str = '/api/v1'
-    PROJECT_NAME: str = 'Auth'
+    MONGO_HOST: str = "localhost"
+    MONGO_PORT: int = 27017
+    MONGO_USER: str = "admin"
+    MONGO_PASS: str = "password"
+    MONGO_DB: str = "auth_db"
+
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "Auth System"
 
     @computed_field
-    def mongo_uri(self) -> MongoDsn:
+    def mongo_uri_computed(self) -> MongoDsn:
+        if self.MONGO_URI:
+            return self.MONGO_URI #type: ignore
+
         return MongoDsn.build(
             scheme='mongodb',
             username=self.MONGO_USER,
